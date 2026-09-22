89801cb11be8f5dfcbae1366fcf468e4065ce34b
Dheeraj Kumawat <dheerajjkumawat@gmail.com>
2026-09-22 12:02:58 +0530

    fix3

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDb() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/todo")
+        await mongoose.connect("mongodb+srv://dheerajjkumawat_db_user:dheerajj%409988..@backend-practice.ddhtdel.mongodb.net/todos")
         console.log("db connected")
     } catch (error) {
         console.log(error)
